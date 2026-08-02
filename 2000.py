a05d96bed143c0dc71141d1e94e651a092c6644e
Monisha <monishasundaram17@gmail.com>
2026-08-02 15:24:08 +0530

    Implemented Reverse Prefix in class solution

---
+++ 2000.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reversePrefix(self, word, ch):
+        if ch not in word:
+            return word
+
+        stack = []
+        answer = []
+
+        for i, c in enumerate(word):
+            stack.append(c)
+
+            if c == ch:
+                answer.extend(reversed(stack))
+                answer.extend(word[i + 1:])
+                break
+
+        return "".join(answer)
