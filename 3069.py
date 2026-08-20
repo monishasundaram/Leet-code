13574d97162c9270174e60805f0f314595c5bb2f
Monisha <monishasundaram17@gmail.com>
2026-08-20 21:51:08 +0530

    Created result array python program

---
+++ 3069.py
@@ -0,0 +1,12 @@
+class Solution:
+    def resultArray(self, nums):
+        a = [nums[0]]
+        b = [nums[1]]
+
+        for i in range(2, len(nums)):
+            if a[-1] > b[-1]:
+                a.append(nums[i])
+            else:
+                b.append(nums[i])
+
+        return a + b
